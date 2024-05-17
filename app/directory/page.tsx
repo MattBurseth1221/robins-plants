014b441dc461e1b68f2b36464b5134a5261fdf34
Matthew Burseth <mattburseth1@gmail.com>
2024-05-16 19:00:58 -0500

    Structure work
    Tailwind not being applied to home page, applies to directory page just fine...?

---
+++ app/directory/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import MainNav from "../_components/MainNav"
+
+export default function Directory() {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <MainNav active={"Home"} />
+    </main>
+  );
+}
