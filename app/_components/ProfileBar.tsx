28b515de4cace74292968fc7a1ac561049a2f9bc
Matthew Burseth <mattburseth1@gmail.com>
2024-05-25 22:54:14 -0500

    Update
    Don't exactly know the best way to check current user out but whatever

--- app/_components/ProfileBar.tsx
+++ app/_components/ProfileBar.tsx
@@ -1,11 +1,9 @@
-'use client'
+"use client";
 
 import { useRouter } from "next/navigation";
 import { useSession } from "../_lib/hooks/SessionContext";
 
 export default function ProfileBar() {
-
-  
   const { user } = useSession();
   const router = useRouter();
 
@@ -27,13 +25,19 @@ export default function ProfileBar() {
   return (
     <nav className="w-40 p-5 border-black border-2 h-64">
       <ul className="flex flex-col justify-between items-center">
-        {user && <p>{ user.username }</p>}
-        {user ?
-            <button className="bg-green-800 text-white p-2 px-4 w-auto rounded-xl hover:bg-green-700 hover:text-black transition" onClick={() => {
-                logout();
-            }}>Log out</button>
-        :
-            <a href="/login">Sign in</a>}
+        {user && <p>{user.username}</p>}
+        {user ? (
+          <button
+            className="bg-green-800 text-white p-2 px-4 w-auto rounded-xl hover:bg-green-700 hover:text-black transition"
+            onClick={() => {
+              logout();
+            }}
+          >
+            Log out
+          </button>
+        ) : (
+          <a href="/login">Sign in</a>
+        )}
       </ul>
     </nav>
   );
