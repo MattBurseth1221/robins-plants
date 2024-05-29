1040f6c421e58baa5e0d1156397150eec0061e26
Matthew Burseth <mattburseth1@gmail.com>
2024-05-29 14:19:37 -0500

    Commit

--- app/_components/Post.tsx
+++ app/_components/Post.tsx
@@ -6,17 +6,23 @@ import Image from "next/image";
 export default function Post({ postInfo }: any) {
   const [post, setPost] = useState<PostType>(postInfo);
 
+  async function getURL() {
+    console.log(await fetch("https://storage.cloud.google.com/robins-plants-photos/Post-Photos/IMG_9610.jpg"));
+  }
+
   return (
     <div className="border-black border-2 bg-slate-100 mb-8 rounded-md text-center p-8 justify-center w-[100%]">
       <Image
         src={
-          "https://hips.hearstapps.com/hmg-prod/images/close-up-of-blossoming-rose-flower-royalty-free-image-1580853844.jpg"
+          "https://t4.ftcdn.net/jpg/05/57/29/25/360_F_557292539_2kXYz0frOcCGISoYEd2MNTmxyT0lYyOj.jpg"
         }
         width="900"
         height="0"
         alt="Flower?"
         className="rounded-md mx-auto"
       />
+
+      <button onClick={ getURL } >Click here</button>
 
       <div className="min-h-16 mt-2">
         <div className="flex flex-row justify-between border-b-[1px] border-slate-500 border-opacity-20">
