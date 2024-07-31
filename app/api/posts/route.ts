b83207c7b039ac558c018a2b6ae26b7b4e4d66e5
Matthew Burseth <mattburseth1@gmail.com>
2024-07-30 21:05:55 -0500

    Working on edits
    Almost done with edit functionality.

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,14 +1,14 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { pool } from "../../_lib/db";
-import { uploadFileToS3 } from "../upload/route"
-import { v4 as uuidv4 } from 'uuid';
+import { uploadFileToS3 } from "../upload/route";
+import { v4 as uuidv4 } from "uuid";
 import { NextApiRequest } from "next";
 
-export async function GET(request: Request) {
-  const limitParam = new URL(request.url).searchParams.get("limit");
-  const sortParam = new URL(request.url).searchParams.get("sortType");
-  const orderParam = new URL(request.url).searchParams.get("order");
-  
+export async function GET(request: NextRequest) {
+  const limitParam = request.nextUrl.searchParams.get("limit");
+  const sortParam = request.nextUrl.searchParams.get("sortType");
+  const orderParam = request.nextUrl.searchParams.get("order");
+
   let sortQuery = "";
   let whereQuery = "";
 
@@ -36,32 +36,26 @@ export async function GET(request: Request) {
 }
 
 export async function POST(request: Request) {
-    console.log('do we get here');
+  console.log("do we get here");
 
   try {
     const formData = await request.formData();
     const postTitle = formData.get("title");
     const postBody = formData.get("body");
 
-    console.log(postTitle, postBody);
-
     const file = formData.get("file") as File;
-
-    console.log('good');
 
     //upload image to S3
     const fileName = await uploadFileToS3(file, file.name);
-
-    console.log(fileName + ' success');
 
     //upload post info to database
     const newUUID = uuidv4();
     const values = [newUUID, postTitle, postBody, fileName];
 
     const query = {
-        text: "INSERT INTO posts(post_id, title, body, image_ref) VALUES($1, $2, $3, $4)",
-        values: values,
-      };
+      text: "INSERT INTO posts(post_id, title, body, image_ref) VALUES($1, $2, $3, $4)",
+      values: values,
+    };
 
     const queryResult = await pool.query(query);
 
@@ -72,21 +66,21 @@ export async function POST(request: Request) {
 }
 
 //FIX REQUEST TYPE
-export async function DELETE(request: any) {
-  console.log('do we get to the delete');
+export async function DELETE(request: NextRequest) {
+  console.log("got to delete");
 
   try {
     const searchParams = request.nextUrl.searchParams;
-    const id = searchParams.get('id');
+    const id = searchParams.get("id");
 
     const query = {
-      text: `DELETE FROM posts WHERE post_id = '${id}'`
+      text: `DELETE FROM posts WHERE post_id = '${id}'`,
     };
 
-  const queryResult = await pool.query(query);
+    const queryResult = await pool.query(query);
 
-  console.log('query result');
-  console.log(queryResult);
+    console.log("query result");
+    console.log(queryResult);
 
     return NextResponse.json({ success: "Post was deleted." });
   } catch (e) {
@@ -94,13 +88,41 @@ export async function DELETE(request: any) {
   }
 }
 
-export async function PUT(request: Request) {
-  const formData = await request.formData();
-  const file = formData.get("file") as File;
-  
-  if (file.size === 0) {
-    
-  }
+export async function PUT(request: NextRequest) {
+  console.log("got to update");
 
-  return;
+  try {
+    const searchParams = request.nextUrl.searchParams;
+    const id = searchParams.get("id");
+
+    const formData = await request.formData();
+    const file = formData.get("file") as File;
+
+    var query = "";
+
+    if (file.size === 0) {
+      query = `UPDATE posts SET title = '${formData.get(
+        "title"
+      )}' AND body = '${formData.get("body")}' WHERE id = '${id}'`;
+
+      console.log(query);
+    } else {
+      //upload image to S3
+      const fileName = await uploadFileToS3(file, file.name);
+
+      console.log(fileName + " success");
+
+      query = `UPDATE posts SET title = '${formData.get(
+        "title"
+      )}' AND body = '${formData.get("body")}' AND image_ref = '${fileName}' WHERE id = '${id}'`;
+    }
+
+    const queryResult = (await pool.query(query)).rows;
+
+    console.log(queryResult);
+
+    return NextResponse.json({ success: "Post updated successfully." });
+  } catch (e) {
+    return NextResponse.json({ error: "Could not update post." });
+  }
 }
