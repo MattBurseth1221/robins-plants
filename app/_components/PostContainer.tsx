5af0bb8ceee3101c2fc65ac6edce48813b97db70
Matthew Burseth <mattburseth1@gmail.com>
2024-11-19 13:30:43 -0600

    Actually fixed loading posts

--- app/_components/PostContainer.tsx
+++ app/_components/PostContainer.tsx
@@ -96,7 +96,7 @@ export default function PostContainer() {
 
     getPosts();
 
-    setLoadingPosts(false);
+    //setLoadingPosts(false);
   }, [filters, sortOrder]);
 
   function toggleSortOrder() {
